9269d921061887b8eaa1ae89aa2f537a7205372d
Nikos Bompetsis <n.bompetsis@opap.gr>
2024-05-03 20:26:38 +0300

    Add models package

---
+++ app/models/models.go
@@ -0,0 +1,21 @@
+package main
+
+import "time"
+
+type List struct {
+	ID      uint      `gorm:"primaryKey"`
+	Name    string    `gorm:"size:100;not null"`
+	Owner   string    `gorm:"size:100;not null"`
+	Created time.Time `gorm:"default:CURRENT_TIMESTAMP;not null"`
+	Active  bool      `gorm:"default:true;not null"`
+	Notes   []Note    `gorm:"many2many:list_notes;"`
+}
+
+type Note struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"size:100;unique;not null"`
+}
+
+type ListNotes struct {
+	Checked bool `gorm:"default:false;not null"`
+}
