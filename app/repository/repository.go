8646591751f5ffe6240f9565ecc85fa56c81c593
Nikos Bompetsis <n.bompetsis@opap.gr>
2024-05-05 14:40:13 +0300

    Fixes on repository package

--- app/repository/repository.go
+++ app/repository/repository.go
@@ -1,18 +1,19 @@
 package repository
 
 import (
-	"log"
+	"errors"
 
 	"github.com/nbompetsis/gin-list-notes/app/models"
 	"gorm.io/gorm"
 )
 
 type ListNotesRepository interface {
-	Save(list models.List)
-	FindById(listId uint) (tags models.List, err error)
+	Save(list models.List) error
+	Update(list *models.List) error
+	FindById(listId uint) (list models.List, err error)
 	FindByOwnerAndActive(owner string, active bool) (lists []models.List, err error)
-	Delete(listId uint)
-	Disable(listId uint)
+	Delete(listId uint) error
+	Disable(listId uint) error
 }
 
 type ListNotesRepositoryImpl struct {
@@ -23,45 +24,54 @@ func NewListNotesRepositoryImpl(db *gorm.DB) ListNotesRepository {
 	return &ListNotesRepositoryImpl{DB: db}
 }
 
-func (repo ListNotesRepositoryImpl) Save(list models.List) {
+func (repo ListNotesRepositoryImpl) Save(list models.List) error {
 	result := repo.DB.Create(&list)
 	if result.Error != nil {
-		log.Fatal(result.Error)
+		return errors.New("list not created")
 	}
+	return nil
+}
+
+func (repo ListNotesRepositoryImpl) Update(list *models.List) error {
+	result := repo.DB.Save(&list)
+	if result.Error != nil {
+		return errors.New("list not updated")
+	}
+	return nil
 }
 
 func (repo ListNotesRepositoryImpl) FindById(listId uint) (l models.List, err error) {
 	var list models.List
 	result := repo.DB.First(&list, "id = ?", listId)
 	if result.Error != nil {
-		return list, nil
-	} else {
-		return list, result.Error
+		return list, errors.New("list not found")
 	}
+	return list, nil
 }
 
 func (repo ListNotesRepositoryImpl) FindByOwnerAndActive(owner string, active bool) (lists []models.List, err error) {
 	var l []models.List
-	result := repo.DB.Where("owner = ? and active = ?", owner, active).Find(&lists)
-	if result.Error != nil {
-		return l, nil
-	} else {
-		return l, result.Error
+	result := repo.DB.Where("owner = ? and active = ?", owner, active).Find(&l)
+	if result.Error != nil || result.RowsAffected == 0 {
+		return l, errors.New("lists not found")
 	}
+	return l, nil
 }
 
-func (repo ListNotesRepositoryImpl) Delete(listId uint) {
+func (repo ListNotesRepositoryImpl) Delete(listId uint) error {
 	var list models.List
 	result := repo.DB.Where("id = ?", listId).Delete(&list)
 	if result.Error != nil {
-		log.Fatal(result.Error)
+		return errors.New("lists not found")
 	}
+	return nil
 }
 
-func (repo ListNotesRepositoryImpl) Disable(listId uint) {
+func (repo ListNotesRepositoryImpl) Disable(listId uint) error {
 	var list models.List
-	result := repo.DB.Model(list).Where("id = ?", listId).Update("active", false)
-	if result.Error != nil {
-		log.Fatal(result.Error)
+	result := repo.DB.Model(list).Where("id = ? and active = true", listId).Update("active", false)
+	if result.Error != nil || result.RowsAffected == 0 {
+		return errors.New("list is not active")
 	}
+	return nil
 }
