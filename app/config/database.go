d19c8d8fab639c06309ea943b80c286ac4fb1a7e
Nikos Bompetsis <n.bompetsis@opap.gr>
2024-05-05 13:20:51 +0300

    Adding package to initiate db connection

---
+++ app/config/database.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connection() *gorm.DB {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("host")
+	user := os.Getenv("user")
+	password := os.Getenv("password")
+	dbName := os.Getenv("dbName")
+	port := os.Getenv("port")
+	timeZone := os.Getenv("timeZone")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=%s", host, user, password, dbName, port, timeZone)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("DB creation error", err)
+	}
+
+	return db
+}
