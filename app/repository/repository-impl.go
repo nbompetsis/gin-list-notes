ce888dd2619b56294e411f4e564be3a9f681147d
Nikos Bompetsis <n.bompetsis@opap.gr>
2024-05-07 13:04:50 +0300

    Adding AddListNotes func and minor renaming

--- app/repository/repository-impl.go
+++ app/repository/repository-impl.go
@@ -21,20 +21,34 @@ func NewListNotesRepositoryImpl() ListNotesRepository {
 
 func (repo ListNotesRepositoryImpl) Save(list models.List) error {
 	// Check if each note exists
+	mergeNotes, err := mergeNotes(repo.DB, list.Notes)
+	if err != nil {
+		return err
+	}
+	list.Notes = mergeNotes
+	result := repo.DB.Create(&list)
+	if result.Error != nil {
+		return errors.New("list not created")
+	}
+	return nil
+}
+
+func mergeNotes(DB *gorm.DB, notes []models.Note) ([]models.Note, error) {
 	var noteNames []string
-	for _, n := range list.Notes {
+	for _, n := range notes {
 		noteNames = append(noteNames, n.Name)
 	}
-	var existedNotes []models.Note
-	if err := repo.DB.Where("name in ? ", noteNames).Find(&existedNotes).Error; err != nil {
-		return errors.New("could not retrive notes")
+	existedNotes, err := findExistedNote(DB, noteNames)
+	if err != nil {
+		return []models.Note{}, err
 	}
+
 	set := make(map[string]bool)
 	for _, element := range existedNotes {
 		set[element.Name] = true
 	}
 	var mergeNotes []models.Note
-	if len(existedNotes) == len(list.Notes) {
+	if len(existedNotes) == len(notes) {
 		mergeNotes = existedNotes
 	} else {
 		for _, element := range noteNames {
@@ -44,12 +58,15 @@ func (repo ListNotesRepositoryImpl) Save(list models.List) error {
 		}
 		mergeNotes = append(mergeNotes, existedNotes...)
 	}
-	list.Notes = mergeNotes
-	result := repo.DB.Create(&list)
-	if result.Error != nil {
-		return errors.New("list not created")
+	return mergeNotes, nil
+}
+
+func findExistedNote(DB *gorm.DB, noteNames []string) ([]models.Note, error) {
+	var existedNotes []models.Note
+	if err := DB.Where("name in ? ", noteNames).Find(&existedNotes).Error; err != nil {
+		return []models.Note{}, errors.New("could not retrive notes")
 	}
-	return nil
+	return existedNotes, nil
 }
 
 func (repo ListNotesRepositoryImpl) Update(listID uint, list models.List) error {
@@ -60,7 +77,22 @@ func (repo ListNotesRepositoryImpl) Update(listID uint, list models.List) error 
 	return nil
 }
 
-func (repo ListNotesRepositoryImpl) FindListNotesByListID(listID uint) (l models.ListNotesInfo, err error) {
+func (repo ListNotesRepositoryImpl) AddListNotes(listID uint, notes []models.Note) error {
+	mergeNotes, err := mergeNotes(repo.DB, notes)
+	if err != nil {
+		return err
+	}
+	var list models.List
+	repo.DB.Where("id=?", listID).Find(&list)
+
+	err = repo.DB.Model(&list).Association("Notes").Replace(mergeNotes)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo ListNotesRepositoryImpl) FindListNotesByListID(listID uint) (models.ListNotesInfo, error) {
 	var listNotes []models.ListNotesInfo
 	result := repo.DB.Raw("SELECT lists.id AS list_id, lists.name AS list_name, notes.id AS note_id, "+
 		"notes.name AS note_name, list_notes.checked AS note_checked FROM lists "+
@@ -72,14 +104,14 @@ func (repo ListNotesRepositoryImpl) FindListNotesByListID(listID uint) (l models
 	return listNotes[0], nil
 }
 
-func (repo ListNotesRepositoryImpl) FindListNotesByOwner(owner string) (l []models.ListNotesInfo, err error) {
+func (repo ListNotesRepositoryImpl) FindListNotesByOwner(owner string) ([]models.ListNotesInfo, error) {
 	var listNotes []models.ListNotesInfo
 	result := repo.DB.Raw("SELECT lists.id AS list_id, lists.name AS list_name, notes.id AS note_id, "+
 		"notes.name AS note_name, list_notes.checked AS note_checked FROM lists "+
 		"INNER JOIN list_notes ON lists.id = list_notes.list_id "+
 		"INNER JOIN notes ON list_notes.note_id = notes.id WHERE lists.owner = ?", owner).Scan(&listNotes)
 	if result.Error != nil || result.RowsAffected == 0 {
-		return l, errors.New("lists not found")
+		return []models.ListNotesInfo{}, errors.New("lists not found")
 	}
 	return listNotes, nil
 }
@@ -93,7 +125,7 @@ func (repo ListNotesRepositoryImpl) DeleteList(listID uint) error {
 	return nil
 }
 
-func (repo ListNotesRepositoryImpl) CheckNote(listID uint, noteID uint) error {
+func (repo ListNotesRepositoryImpl) CheckListNote(listID uint, noteID uint) error {
 	result := repo.DB.Model(&models.ListNotes{}).Where("list_id = ? AND note_id = ? AND checked = false", listID, noteID).Update("checked", true)
 	if result.Error != nil || result.RowsAffected == 0 {
 		return fmt.Errorf("note %d is already checked or not found for the list %d", noteID, listID)
@@ -101,7 +133,7 @@ func (repo ListNotesRepositoryImpl) CheckNote(listID uint, noteID uint) error {
 	return nil
 }
 
-func (repo ListNotesRepositoryImpl) CheckAllNotes(listID uint) error {
+func (repo ListNotesRepositoryImpl) CheckListAllNotes(listID uint) error {
 	result := repo.DB.Model(&models.ListNotes{}).Where("list_id = ?", listID).Update("checked", true)
 	if result.Error != nil || result.RowsAffected == 0 {
 		return fmt.Errorf("notes are already checked or list %d not found", listID)
--- app/repository/repository.go
+++ app/repository/repository.go
@@ -7,9 +7,10 @@ import (
 type ListNotesRepository interface {
 	Save(list models.List) error
 	Update(listId uint, updatedList models.List) error
+	AddListNotes(listID uint, notes []models.Note) error
 	FindListNotesByListID(listID uint) (listNotesInfo models.ListNotesInfo, err error)
 	FindListNotesByOwner(owner string) (listNotesInfo []models.ListNotesInfo, err error)
 	DeleteList(listID uint) error
-	CheckNote(listID uint, noteID uint) error
-	CheckAllNotes(listID uint) error
+	CheckListNote(listID uint, noteID uint) error
+	CheckListAllNotes(listID uint) error
 }
